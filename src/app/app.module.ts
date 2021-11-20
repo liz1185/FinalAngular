ccc2a17cf754442858e0d8b99f9d4aceae53d9f6
liz1185 <cardozoliz1185@gmail.com>
2021-11-20 01:05:30 -0300

    hacer pagina de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
-// import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-// import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ReactiveFormComponent
+
 
   ],
   imports: [
@@ -21,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     CoreModule,
     HttpClientModule,
-    // ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
