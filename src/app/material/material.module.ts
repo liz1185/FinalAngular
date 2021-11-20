46c4283f0847b97d5dff56842990231acfde099e
liz1185 <cardozoliz1185@gmail.com>
2021-11-20 04:15:16 -0300

    trabajo terminado

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
 import {MatBadgeModule} from '@angular/material/badge';
 
+
 let materialModules=[
   MatButtonModule,
   MatCardModule,
@@ -27,11 +28,12 @@ let materialModules=[
   MatToolbarModule,
   MatTableModule,
   MatBadgeModule,
+
 ]
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, materialModules], 
+  imports: [CommonModule, materialModules],
   exports: [materialModules],
 })
 export class MaterialModule { }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+
+
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatCardModule,
+
+
+  ],
 })
 export class HomeModule { }
